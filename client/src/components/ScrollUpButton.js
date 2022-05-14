52f4eeef194b5de073c8583cd35ae140f2de6028
Sanae G <sanae.gassa@gmail.com>
2022-05-14 12:44:14 +0200

    use prettier

--- client/src/components/ScrollUpButton.js
+++ client/src/components/ScrollUpButton.js
@@ -4,37 +4,39 @@ import { useEffect, useState } from 'react';
 function ScrollUpButton() {
   const [buttonState, setButtonState] = useState(false);
 
-  useEffect (() => {
+  useEffect(() => {
     window.addEventListener('scroll', () => {
-      if(window.scrollY > 300) {
+      if (window.scrollY > 300) {
         setButtonState(true);
       } else {
         setButtonState(false);
       }
-    })
-  }, [])
+    });
+  }, []);
 
   const scrollUp = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
-    })
-  }
+      behavior: 'smooth',
+    });
+  };
 
   return (
-
     buttonState && (
-      <button style={{
-        position: 'fixed',
-        bottom: '80px',
-        left: '50px',
-        height: '50px',
-        width: '50px'
-      }} onClick={scrollUp}>Up</button>
+      <button
+        style={{
+          position: 'fixed',
+          bottom: '80px',
+          left: '50px',
+          height: '50px',
+          width: '50px',
+        }}
+        onClick={scrollUp}
+      >
+        Up
+      </button>
     )
-   
-    
-    )
+  );
 }
 
 export default ScrollUpButton;
