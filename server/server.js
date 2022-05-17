950885d083afe8174b29bfc0f7204cc43be5d835
Sanae G <sanae.gassa@gmail.com>
2022-05-17 13:32:44 +0200

    add cors

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 
 import api from './routes/api.js';
@@ -21,6 +22,7 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = process.env.PORT || 5007;
 
+app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
